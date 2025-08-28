250cf95569af32cdbcd7ae354cd04b2eec9e0898
CarlosRial-23 <rialcarlos2009@hotmail.com>
2025-08-28 01:17:13 -0300

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "bienvenida",
+        pathMatch: 'full'
+    },
+    {
+        path: "bienvenida",
+        loadComponent: () => import("./pages/bienvenida/bienvenida").then((c)=> c.Bienvenida)
+    },
+    {
+        path: "login",
+        loadComponent: () => import("./pages/login/login").then((c)=> c.Login )
+    },
+    {
+        path: "registro",
+        loadComponent: () => import("./pages/registro/registro").then((c)=> c.Registro )
+    },
+    {
+        path: "error",
+        loadComponent: () => import("./pages/error/error").then((c)=> c.Error )
+    },
+    {
+        path: "sobre-mi",
+        loadComponent: () => import("./pages/sobre-mi/sobre-mi").then((c)=> c.SobreMi )
+    },
+
+    {
+        path: "**",
+        redirectTo: "error"
+    }
+
+];
